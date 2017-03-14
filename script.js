7ab698ca7c6b817cf595d79a2a014a608f8d6d48
shabat <kyrkela21@gmail.com>
2017-03-14 21:39:40 +0200

    Its a mess, but not for long

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-const CHART = document.getElementById('lineChart');
-let lineChart = new Chart(CHART, {
-    type: 'line',
-    data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            showScale: false,
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "#fff",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderWidth: 5,
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 15,
-            pointHoverRadius: 15,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
-        }
-    ]
-},
+var input = $('#city');
+var button = $('#submit');
+button.on('click', function (){
+    var cityName= input.val();
+    var key = '19ad4402d0b34d51825115807170903';
+    var days = '7';
 
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.apixu.com/v1/forecast.json?key='+ key +'&q='+ cityName +'&days='+days,
+        success: function(data){
+            console.log(data);
+
+        }
+    })
+});
+
+$("#city").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#submit").click();
+    }
 });
