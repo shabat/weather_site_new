6c98674dc2c18350e755584882c79a53bcf0d432
shabat <kyrkela21@gmail.com>
2017-03-15 22:39:58 +0200

    I'm terrible at commit messages(

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 var input = $('#city');
 var button = $('#submit');
+var cityName = 'auto:ip';
 button.on('click', function (){
-    var cityName= input.val();
+    cityName= input.val();
     var key = '19ad4402d0b34d51825115807170903';
     var days = '7';
-
-
     $.ajax({
         type: 'GET',
         url: 'https://api.apixu.com/v1/forecast.json?key='+ key +'&q='+ cityName +'&days='+days,
         success: function(data){
             console.log(data);
-
+            $('#country').html(data.location.country);
+            $('#temp').html(data.current.temp_c +'&deg;C');
+            $('#town').html(data.location.name);
         }
     })
 });
 
-$("#city").keyup(function(event){
+$(function () {
+    var key = '19ad4402d0b34d51825115807170903';
+    var days = '7';
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.apixu.com/v1/forecast.json?key='+ key +'&q='+ cityName +'&days='+days,
+        success: function(data){
+            console.log(data);
+            $('#country').html(data.location.country);
+            $('#temp').html(data.current.temp_c +'&deg;C');
+            $('#town').html(data.location.name);
+            $('#tdate').html(data.location.localtime);
+        }
+    })
+});
+
+input.keyup(function(event){
     if(event.keyCode == 13){
         $("#submit").click();
     }
 });
+
+/*
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            }
+        }
+        function showPosition(position) {
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+        }
+        getLocation();
+*/
